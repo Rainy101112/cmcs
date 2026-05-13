b36535e76a85da1cf5900420192ee10f0521a989
Rainy101112 <Rainy101112@163.com>
2026-05-14 01:18:55 +0800

    Use epoll to process network connection

--- main.c
+++ main.c
@@ -1,133 +1,178 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <unistd.h>
+#include <errno.h>
+
+#include <fcntl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/epoll.h>
 
-#include "varint.h"
-#include "handshake.h"
 #include "protocol.h"
+#include "varint.h"
+#include "netbuf.h"
 #include "packet.h"
 
+#define MAX_EVENTS 64
 #define PORT 25565
 
-int start_server();
-void run_event_loop(int server_fd);
-void process_client_packets(client_connection *conn);
+void set_nonblocking(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
 
 int start_server() {
     int server_fd;
     struct sockaddr_in address;
     int opt = 1;
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        return -1;
-    }
-
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    
+    set_nonblocking(server_fd);
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        return -1;
-    }
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, SOMAXCONN);
 
-    if (listen(server_fd, 3) < 0) {
-        perror("listen failed");
-        return -1;
-    }
-
-    printf("Minecraft C Server started on port %d\n", PORT);
+    printf("Minecraft server started on port %d\n", PORT);
     return server_fd;
 }
 
-void run_event_loop(int server_fd) {
-    int client_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-
-    while (1) {
-        if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");
-            continue;
-        }
-
-        printf("Client connected!\n");
-
-        /* Initialize connection context */
-        client_connection conn;
-        conn.socket = client_socket;
-        conn.state = STATE_HANDSHAKING;
-        conn.in_buf = netbuf_new(4096);
-        conn.player_name = NULL;
-        conn.uuid = NULL;
-
-        process_client_packets(&conn); 
-
-        close(client_socket);
-        netbuf_free(conn.in_buf);
-        printf("Client disconnected.\n");
-    }
-}
-
-void process_client_packets(client_connection *conn) {
+int process_client_packets(client_connection *conn) {
     int client_socket = conn->socket;
     netbuf *in_buf = conn->in_buf;
 
     while (1) {
-        /* Check buffer remaining space, do not read if too small,
-         or we'll get out of boundary */
+        /* Check buffer remaining space */
         if (in_buf->capacity - in_buf->size < 128) {
-             // TODO: Expand capicity
-             break; 
+            // TODO: realloc
+            break; 
         }
 
-        ssize_t bytes_received = recv(client_socket, in_buf->data + in_buf->size, 
+        ssize_t bytes_received = recv(client_socket, 
+                                     in_buf->data + in_buf->size, 
                                      in_buf->capacity - in_buf->size, 0);
         
-        if (bytes_received <= 0) break; 
-        in_buf->size += bytes_received;
-
-        while (1) {
-            size_t save_offset = in_buf->offset;
-            in_buf->error = false; 
-            
-            varint packet_len = read_varint(in_buf);
-
-            if (in_buf->error) {
-                in_buf->offset = save_offset;
-                break;
+        if (bytes_received < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                /* Read out */
+                break; 
             }
-
-            if (in_buf->size - in_buf->offset < (size_t)packet_len) {
-                in_buf->offset = save_offset;
-                break;
-            }
-
-            size_t packet_end = in_buf->offset + packet_len;
-
-            handle_packet(conn); 
-
-            in_buf->offset = packet_end;
-            if (in_buf->offset >= in_buf->size) break;
+            perror("recv error");
+            return -1; // Real network error
+        } else if (bytes_received == 0) {
+            return 0; // Disconnect
         }
-        netbuf_compact(in_buf);
+
+        in_buf->size += bytes_received;
+    }
+
+    while (1) {
+        size_t save_offset = in_buf->offset;
+        in_buf->error = false; 
+        
+        /* Get length */
+        varint packet_len = read_varint(in_buf);
+
+        if (in_buf->error) {
+            in_buf->offset = save_offset;
+            break;
+        }
+
+        if (in_buf->size - in_buf->offset < (size_t)packet_len) {
+            in_buf->offset = save_offset;
+            break;
+        }
+
+        /* Record current packet boundary */
+        size_t packet_end = in_buf->offset + packet_len;
+
+        /* Handle... */
+        handle_packet(conn); 
+
+        /* Force align */
+        in_buf->offset = packet_end;
+
+        /* Read out */
+        if (in_buf->offset >= in_buf->size) {
+            break;
+        }
+    }
+
+    netbuf_compact(in_buf);
+
+    return 1;
+}
+
+void run_epoll_loop(int server_fd) {
+    int epoll_fd = epoll_create1(0);
+    struct epoll_event ev, events[MAX_EVENTS];
+
+    ev.events = EPOLLIN;
+    ev.data.fd = server_fd; 
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev);
+
+    while (1) {
+        int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        
+        for (int i = 0; i < nfds; i++) {
+            if (events[i].data.fd == server_fd) {
+                /* Process new connection */
+                struct sockaddr_in client_addr;
+                socklen_t addr_len = sizeof(client_addr);
+                int client_sock = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+                
+                if (client_sock == -1) continue;
+
+                set_nonblocking(client_sock);
+                
+                /* Initialize connection context */
+                client_connection *conn = malloc(sizeof(client_connection));
+                conn->socket = client_sock;
+                conn->state = STATE_HANDSHAKING;
+                conn->in_buf = netbuf_new(4096);
+                conn->player_name = NULL;
+                conn->uuid = NULL;
+
+                ev.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+                ev.data.ptr = conn;
+                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_sock, &ev);
+                
+                printf("New connection: FD %d\n", client_sock);
+
+            } else {
+                /* Process client data */
+                client_connection *conn = (client_connection *)events[i].data.ptr;
+
+                /* Check if disconnect event */
+                if (events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR)) {
+                    goto disconnect;
+                }
+
+                int status = process_client_packets(conn);
+                
+                if (status <= 0) {
+                disconnect:
+                    printf("Client FD %d disconnected.\n", conn->socket);
+                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, conn->socket, NULL);
+                    close(conn->socket);
+                    if (conn->player_name) free(conn->player_name);
+                    netbuf_free(conn->in_buf);
+                    free(conn);
+                }
+            }
+        }
     }
 }
 
-int main(){
+int main() {
     int server_fd = start_server();
-    if (server_fd < 0) {
-        return 0;
-    }
-
-    run_event_loop(server_fd);
-
+    if (server_fd < 0) return 1;
+    run_epoll_loop(server_fd);
     return 0;
 }
